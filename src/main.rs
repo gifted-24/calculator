f8b2cacf72c6057b704c1af960699f635535e6ce
gift esefo <giftesefo78@gmail.com>
2025-12-29 21:12:56 +0000

    basic calculator features implemented and tested

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,60 @@
+use std::env::args;
+
+struct Console {
+    first_num: f32,
+    second_num: f32,
+    operator: char,
+    result: f32,
+    error: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut console: Console = Console::new(args().collect());
+    console.calculator();
+    console.output();
+}
+
+impl Console {
+    fn new(arg: Vec<String>) -> Console {
+        Console {
+            first_num: arg[1].parse().unwrap(),
+            second_num: arg[3].parse().unwrap(),
+            operator: arg[2].parse().unwrap(),
+            result: 0.0,
+            error: String::new(),
+        }
+    }
+
+    fn calculator(&mut self) {
+        match self.operator {
+            '+' => {
+                self.result = self.first_num + self.second_num;
+            }
+            '-' => {
+                self.result = self.first_num - self.second_num;
+            }
+            '*' | 'x' | 'X' => {
+                self.result = self.first_num * self.second_num;
+            }
+            '/' => {
+                self.result = self.first_num / self.second_num;
+            }
+            _ => {
+                self.error = format!(
+                    "Invalid Operation: [{} {} {}]",
+                    self.first_num, self.operator, self.second_num
+                );
+            }
+        }
+    }
+
+    fn output(&self) {
+        match self.result {
+            0.0 => println!("{}", self.error),
+            _ => println!(
+                "{} {} {} = {}",
+                self.first_num, self.operator, self.second_num, self.result
+            ),
+        }
+    }
 }
